110f0d509c6e25a3b9e9877bdc8e1cff33094995
ArielMAJ <69123486+ArielMAJ@users.noreply.github.com>
2022-05-10 05:04:51 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main(void)
+{#include <stdio.h>
+
+int main(void)
 {
-	printf("%.1f", 2.25);
+	float x = 2.25;
+	
+	// Should print 2.2 on -std=c11
+	printf("%.1f\n", x);
+	
+	// Should print 2.2 on -std=c11
+	printf("%.1f\n", 2.25);
+	
+	// Should print 4
+	printf("%d\n", sizeof(float));
+	
 	return 0;
 }
